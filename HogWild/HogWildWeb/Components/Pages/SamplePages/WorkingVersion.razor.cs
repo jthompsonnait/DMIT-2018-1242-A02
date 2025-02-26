c2fc497581270ab8c18e2082ea70326ee5765fa6
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2025-02-26 13:57:41 -0700

    Changes since Bel check in

--- HogWild/HogWildWeb/Components/Pages/SamplePages/WorkingVersion.razor.cs
+++ HogWild/HogWildWeb/Components/Pages/SamplePages/WorkingVersion.razor.cs
@@ -1,7 +1,62 @@
-﻿namespace HogWildWeb.Components.Pages.SamplePages
+﻿using HogWildSystem.BLL;
+using HogWildSystem.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace HogWildWeb.Components.Pages.SamplePages
 {
     public partial class WorkingVersion
 
     {
+        #region Fields
+        //  Property for holding any feedback messages
+        private string feedback = string.Empty;
+        // This private field holds a reference to the WorkingVersionsView instance.
+        private WorkingVersionsView? workingVersionView = new WorkingVersionsView();
+        #endregion
+
+        #region Properties
+        // This attribute marks the property for dependency injection.
+        [Inject]
+        // This property provides access to the 'WorkingVersionService' service.
+        protected WorkingVersionsService WorkingVersionService { get; set; } = default!;
+        #endregion
+
+        #region Methods
+        private void GetWorkingVersion()
+        {
+            try
+            {
+                workingVersionView = WorkingVersionService.GetWorkingVersion();
+            }
+            #region catch all exceptions
+            catch (AggregateException ex)
+            {
+                foreach (var error in ex.InnerExceptions)
+                {
+                    feedback = error.Message;
+                }
+            }
+
+            catch (ArgumentNullException ex)
+            {
+                feedback = GetInnerException(ex).Message;
+            }
+
+            catch (Exception ex)
+            {
+                feedback = GetInnerException(ex).Message;
+            }
+            #endregion
+
+        }
+        private Exception GetInnerException(Exception ex)
+        {
+            while (ex.InnerException != null)
+                ex = ex.InnerException;
+            return ex;
+        }
+
+#endregion
     }
 }
+
