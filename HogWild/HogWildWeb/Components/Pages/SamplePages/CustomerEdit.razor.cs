d7911c9e5a57c048bc717aeee149babdc140e48e
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2025-03-10 09:43:51 -0600

    End of class

--- HogWild/HogWildWeb/Components/Pages/SamplePages/CustomerEdit.razor.cs
+++ HogWild/HogWildWeb/Components/Pages/SamplePages/CustomerEdit.razor.cs
@@ -1,11 +1,21 @@
 ﻿using System.Globalization;
 using HogWildSystem.BLL;
+using HogWildSystem.ViewModels;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MudBlazor;
+
 
 namespace HogWildWeb.Components.Pages.SamplePages
 {
     public partial class CustomerEdit
     {
+        #region Fields
+        // The customer
+        private CustomerEditView customer = new();
+        //  mudform control
+        private MudForm customerForm = new();
+        #endregion
         #region Feedback & Error Messages
         // The feedback message
         private string feedbackMessage = string.Empty;
@@ -28,6 +38,55 @@ namespace HogWildWeb.Components.Pages.SamplePages
 
         //  Customer ID used to create or edit a customer
         [Parameter] public int CustomerID { get; set; } = 0;
+        #endregion
+
+        #region Methods
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            try
+            {
+                //  reset the error detail list
+                errorDetails.Clear();
+
+                //  reset the error message to an empty string
+                errorMessage = string.Empty;
+
+                //  reset feedback message to an empty string
+                feedbackMessage = String.Empty;
+                //  check to see if we are navigating using a valid customer CustomerID.
+                //      or are we going to create a new customer.
+                if (CustomerID > 0)
+                {
+                    customer = CustomerService.GetCustomer(CustomerID) ?? new();
+                }
+
+                StateHasChanged();
+            }
+            catch (ArgumentNullException ex)
+            {
+                errorMessage = BlazorHelperClass.GetInnerException(ex).Message;
+            }
+            catch (ArgumentException ex)
+            {
+                errorMessage = BlazorHelperClass.GetInnerException(ex).Message;
+            }
+            catch (AggregateException ex)
+            {
+                //  have a collection of errors
+                //  each error should be place into a separate line
+                if (!string.IsNullOrWhiteSpace(errorMessage))
+                {
+                    errorMessage = $"{errorMessage}{Environment.NewLine}";
+                }
+
+                errorMessage = $"{errorMessage}Unable to search for customer";
+                foreach (var error in ex.InnerExceptions)
+                {
+                    errorDetails.Add(error.Message);
+                }
+            }
+        }
         #endregion
     }
 }
